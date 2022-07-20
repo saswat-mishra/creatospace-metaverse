247cb999962cae472fefe36ff53bfdcb29050d4e
Adityaarora08 <79107244+Adityaarora08@users.noreply.github.com>
2022-07-20 09:12:16 +0530

    OAuth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,20 @@ import Register from "./Views/Register";
 import Room from "./Views/Room";
 import React from "react";
 import RestrictedRoute from "./utils/RestrictedRoutes";
+import { useEffect } from 'react';
+import { gapi } from 'gapi-script';
 const client_id =
   "215504042537-v5rsk5ss23ktuo28ipe0uq9lm6s01t1i.apps.googleusercontent.com";
 function App() {
+  useEffect(() => {
+    function start(){
+      gapi.auth2.init({
+        client_id: client_id,
+        scope: ""
+      })
+    };
+    gapi.load('client: auth2',start);
+  });
   return (
     <div>
       <Routes>
